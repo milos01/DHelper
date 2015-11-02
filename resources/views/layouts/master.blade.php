70874e905d722412363fe79757c92e0d3e445101
milos01 <milosa942@gmail.com>
2015-11-02 23:05:08 +0100

    DHelper

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Danulabs helper</title>
+    @section('head')
+      <link rel="stylesheet" href="{{ URL::asset('../css/style.css') }}"/>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"/>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"/>
+    @show
+  </head>
+  
+<body>
+@section('body')
+  
+@show
+@section('javascript')
+  <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+@show
+</body>
+</html>
